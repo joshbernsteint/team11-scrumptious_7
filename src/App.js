54e3690140d949f42e8954f7a75b04da01728202
sohanchatterjee <74262451+sohanchatterjee@users.noreply.github.com>
2023-03-05 20:49:16 -0500

    Update App.js
    Import notifications

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import CameraAccess from './components/CameraAccess';
+import MaterialNotification from './components/MaterialNotification';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <CameraAccess></CameraAccess>
+        <MaterialNotification></MaterialNotification>
       </header>
     </div>
   );
